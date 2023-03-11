df5a2d5f1a0d40b6f1ba330d457c20fee915f5ed
Yaser Kazerooni <yaser.kazerooni@outlook.com>
2023-03-11 23:43:53 +0100

    Add test units for PetApi

--- jersey-swagger-codegen-api-client/src/test/java/de/kaziyas/petshop/client/api/PetApiTest.java
+++ jersey-swagger-codegen-api-client/src/test/java/de/kaziyas/petshop/client/api/PetApiTest.java
@@ -1,32 +1,50 @@
 package de.kaziyas.petshop.client.api;
 
+import de.kaziyas.petshop.client.invoker.ApiException;
+import de.kaziyas.petshop.client.model.Category;
 import de.kaziyas.petshop.client.model.ModelApiResponse;
 import de.kaziyas.petshop.client.model.Pet;
-import org.junit.Ignore;
-import org.junit.Test;
+import de.kaziyas.petshop.client.model.Tag;
+import org.junit.*;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * API tests for PetApi
  */
-@Ignore
 public class PetApiTest {
 
     private final PetApi api = new PetApi();
+
+    private Pet pet;
+    private List<String> photoUrls;
+    private Category category;
+    private List<Tag> tags;
 
     /**
      * Add a new pet to the store
      *
      * @throws Exception if the Api call fails
      */
+
+    @Before
+    public void setup() {
+        pet = createPet();
+        photoUrls = createPhotoUrls();
+        category = createCategory();
+        tags = createTags();
+    }
+
     @Test
     public void addPetTest() throws Exception {
-        Pet body = null;
+        Pet body = pet;
         api.addPet(body);
 
-        // TODO: test validations
+        Pet response = api.getPetById(pet.getId());
+        Assert.assertEquals(pet.getId(), response.getId());
     }
 
     /**
@@ -34,13 +52,16 @@ public class PetApiTest {
      *
      * @throws Exception if the Api call fails
      */
-    @Test
+    @Test(expected = ApiException.class)
     public void deletePetTest() throws Exception {
-        Long petId = null;
+        Pet pet1 = createPet1();
+        api.addPet(pet1);
+
+        Long petId = pet1.getId();
         String apiKey = null;
         api.deletePet(petId, apiKey);
 
-        // TODO: test validations
+        api.getPetById(petId);
     }
 
     /**
@@ -52,10 +73,15 @@ public class PetApiTest {
      */
     @Test
     public void findPetsByStatusTest() throws Exception {
-        List<String> status = null;
-        List<Pet> response = api.findPetsByStatus(status);
+        List<String> status = new ArrayList<>();
+        status.add(Pet.StatusEnum.AVAILABLE.getValue());
+        status.add(Pet.StatusEnum.SOLD.getValue());
 
-        // TODO: test validations
+        List<Pet> response = api.findPetsByStatus(status);
+        Assert.assertEquals(Pet.StatusEnum.AVAILABLE, response.get(0).getStatus());
+
+        List<Pet> invalidPets = response.stream().filter(e -> e.getStatus().equals(Pet.StatusEnum.PENDING)).collect(Collectors.toList());
+        Assert.assertEquals(0, invalidPets.size());
     }
 
     /**
@@ -67,10 +93,10 @@ public class PetApiTest {
      */
     @Test
     public void getPetByIdTest() throws Exception {
-        Long petId = null;
+        Long petId = 22408871L;
         Pet response = api.getPetById(petId);
 
-        // TODO: test validations
+        Assert.assertEquals(pet.getId(), response.getId());
     }
 
     /**
@@ -80,10 +106,12 @@ public class PetApiTest {
      */
     @Test
     public void updatePetTest() throws Exception {
-        Pet body = null;
+        Pet body = pet;
+        body.setName("Tiger");
         api.updatePet(body);
 
-        // TODO: test validations
+        Pet response = api.getPetById(body.getId());
+        Assert.assertEquals("Tiger", response.getName());
     }
 
     /**
@@ -93,12 +121,16 @@ public class PetApiTest {
      */
     @Test
     public void updatePetWithFormTest() throws Exception {
-        Long petId = null;
-        String name = null;
-        String status = null;
+        Long petId = pet.getId();
+        String name = "Lion";
+        String status = Pet.StatusEnum.SOLD.getValue();
         api.updatePetWithForm(petId, name, status);
 
-        // TODO: test validations
+        Pet response = api.getPetById(petId);
+
+        Assert.assertEquals(name, response.getName());
+        Assert.assertNotEquals(pet.getName(), response.getName());
+        Assert.assertNotEquals(pet.getStatus(), response.getStatus());
     }
 
     /**
@@ -108,12 +140,65 @@ public class PetApiTest {
      */
     @Test
     public void uploadFileTest() throws Exception {
-        Long petId = null;
-        String additionalMetadata = null;
-        File file = null;
-        ModelApiResponse response = api.uploadFile(petId, additionalMetadata, file);
+        Long petId = pet.getId();
+        String additionalMetadata = "this is a test for upload file service";
 
-        // TODO: test validations
+        File file = new File("sample.txt");
+     //   ModelApiResponse response = api.uploadFile(petId, additionalMetadata, file);
+    }
+
+    private Pet createPet() {
+        Pet pet = new Pet();
+        pet.setId(22408871L);
+        pet.setName("doggie");
+        pet.setStatus(Pet.StatusEnum.AVAILABLE);
+
+        pet.setPhotoUrls(photoUrls);
+        pet.setCategory(category);
+        pet.setTags(tags);
+        return pet;
+    }
+
+    private Pet createPet1() {
+        Pet pet = new Pet();
+        pet.setId(19810821L);
+        pet.setName("ant");
+        pet.setStatus(Pet.StatusEnum.SOLD);
+
+        pet.setPhotoUrls(photoUrls);
+        pet.setCategory(category);
+        pet.setTags(tags);
+        return pet;
+    }
+
+    private List<String> createPhotoUrls() {
+        List<String> photoUrls = new ArrayList<>();
+        photoUrls.add("ea nostrud occaecat reprehenderit");
+        photoUrls.add("consequat eu dolore");
+        return photoUrls;
+    }
+
+    private Category createCategory() {
+        Category category = new Category();
+        category.setId(27710153L);
+        category.setName("in ipsum enim");
+        return category;
+    }
+
+    private List<Tag> createTags() {
+        Tag tag = new Tag();
+        tag.setId(-68369270L);
+        tag.setName("veniam reprehenderit");
+
+        Tag tag1 = new Tag();
+        tag1.setId(-76889784L);
+        tag.setName("anim adipisicing veniam minim");
+
+        List<Tag> tags = new ArrayList();
+        tags.add(tag);
+        tags.add(tag1);
+
+        return tags;
     }
 
 }
